fbadcd77d8a595765d72166722c8380940e8b360
Kazi Shariful Islam <kazisharifulislam52@gmail.com>
2022-05-23 01:43:08 +0600

    post is not working

--- src/pages/checkout/Purchase.js
+++ src/pages/checkout/Purchase.js
@@ -4,37 +4,76 @@ import { useParams } from "react-router-dom";
 
 const Purchase = () => {
   const { pId } = useParams();
-  //   console.log(pId);
   const [product, setProduct] = useState([]);
   useEffect(() => {
     axios
-      .get(`http://localhost:5000/product/${pId}`)
+      .get(`https://manufacturer-website-ks.herokuapp.com/product/${pId}`)
       .then((res) => setProduct(res.data));
   }, [pId]);
-  console.log(product);
- 
+
+
+const {name, price, img, min_quan, avail} = product;
+  const [quantity, setQuantity] = useState(parseInt(product.min_quan));
+  const placeOrder = () => {
+    // create new order object 
+    let order = { name, quantity: quantity || min_quan, img, price };
+    const total_price =  order.quantity * order.price;
+    order = {...order, total_price}
+    console.log(order);
+    axios.post('http://localhost/new-order', order).then(res => console.log(res))
+
+  };
+  // useEffect(()=> {
+  // }, [])
+
   return (
     <div className="px-2">
-      <h3 className="text-3xl text-center text-dark py-12 px-4">Order Product</h3>
+      <h3 className="text-3xl text-center text-dark py-12 px-4">
+        Order Product
+      </h3>
 
-      <div class="card max-w-4xl mx-auto md:card-side bg-base-100 shadow-xl">
-      <figure><img className="p-4" src={product.img} alt="Album" /></figure>
-      <div class="card-body flex items-center justify-center">
-        <h2 class="card-title">{product.name}</h2>
-           <div class="card-actions items-center justify-around">
-              <div className=""><p>Price: ${product.price}<sub> /item</sub></p></div>
-              <div className="flex gap-4 py-2">
-              <div className="badge badge-outline tooltip" data-tip="Min order quantity">Min: {product.min_quan}</div>
-              <div className="badge badge-outline tooltip" data-tip="Available quantity">Av: {product.avail}</div>
+      <div className="card max-w-4xl mx-auto md:card-side bg-base-100 shadow-xl">
+        <figure>
+          <img className="p-4" src={img} alt="Album" />
+        </figure>
+        <div className="card-body flex items-center justify-center">
+          <h2 className="card-title">{name}</h2>
+          <div className="card-actions items-center justify-around">
+            <div className="">
+              <p>
+                Price: ${price}
+                <sub> /item</sub>
+              </p>
+            </div>
+            <div className="flex gap-4 py-2">
+              <div
+                className="badge badge-outline tooltip"
+                data-tip="Min order quantity"
+              >
+                Min: {min_quan}
               </div>
+              <div
+                className="badge badge-outline tooltip"
+                data-tip="Available quantity"
+              >
+                Av: {avail}
+              </div>
+            </div>
           </div>
           <div className="flex items-center justify-between gap-4">
-            <input type="number" defaultValue={product.min_quan} placeholder="Order Quantity" class="input input-bordered w-full max-w-md" />
-            <button className="btn btn-secondary">Place Order</button>
+            <input
+              onChange={(e) => setQuantity(parseInt(e.target.value))}
+              type="number"
+              defaultValue={min_quan}
+              placeholder="Order Quantity"
+              className="input input-bordered w-full max-w-md"
+            />
+            <button onClick={placeOrder} className="btn btn-secondary">
+              Place Order
+            </button>
           </div>
+        </div>
       </div>
-    </div>
-
     </div>
   );
 };
--- src/pages/home/FeaturedProducts.js
+++ src/pages/home/FeaturedProducts.js
@@ -29,7 +29,7 @@ const FeaturedProducts = () => {
   
   const [products, setProducts] = useState([])
   useEffect(()=> {
-      fetch('http://localhost:5000/products')
+      fetch('https://manufacturer-website-ks.herokuapp.com/products')
       .then(res => res.json())
       .then(data=> {
           console.log(data);
